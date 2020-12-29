b1843096941e62e75b82cc019a6693f77796bfb1
cvrppradeep <43846840+cvrppradeep@users.noreply.github.com>
2020-12-30 02:58:14 +0530

    my

---
+++ build/precache-manifest.93e73fe53fc71d90b7bc139ebbeb708b.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe3525d31541732dabdf374d41221d2b",
+    "url": "/Myweb/index.html"
+  },
+  {
+    "revision": "c40a12e5f40427111ede",
+    "url": "/Myweb/static/css/main.9229f7b0.chunk.css"
+  },
+  {
+    "revision": "9005badb76f126c7a706",
+    "url": "/Myweb/static/js/2.d0b34b90.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/Myweb/static/js/2.d0b34b90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c40a12e5f40427111ede",
+    "url": "/Myweb/static/js/main.ee40a10f.chunk.js"
+  },
+  {
+    "revision": "2ae97f64ed911469dfbd",
+    "url": "/Myweb/static/js/runtime-main.ab4e63af.js"
+  },
+  {
+    "revision": "114427de4c2b3a67d47af402967a3263",
+    "url": "/Myweb/static/media/FoodingHub.114427de.png"
+  },
+  {
+    "revision": "f482e00fd4d7a5ffa30c36a2d3fc27b7",
+    "url": "/Myweb/static/media/Kashware.f482e00f.png"
+  },
+  {
+    "revision": "8c9ffe8c9b3f49b1c813f704c81da08d",
+    "url": "/Myweb/static/media/The Next Cart.8c9ffe8c.png"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "/Myweb/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/Myweb/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/Myweb/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/Myweb/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "add239172d50d6ad18d2ac3cea6257a1",
+    "url": "/Myweb/static/media/digital-ocean.add23917.svg"
+  },
+  {
+    "revision": "bcf22073c9f13bab1761b90bce9fec55",
+    "url": "/Myweb/static/media/django.bcf22073.svg"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/Myweb/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "d98463abd102acdc2b1eb8d5eb70b2d2",
+    "url": "/Myweb/static/media/flask.d98463ab.svg"
+  },
+  {
+    "revision": "0692ddac9614e47c40a43276cd9287e4",
+    "url": "/Myweb/static/media/get_github_info.0692ddac.webp"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/Myweb/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "475c8d36922ab026b45e7db28e023590",
+    "url": "/Myweb/static/media/github-api.475c8d36.svg"
+  },
+  {
+    "revision": "567f006b9ca974f343db1c9c79920074",
+    "url": "/Myweb/static/media/github.567f006b.svg"
+  },
+  {
+    "revision": "30bd42ea97a2e0454bb1dfbab983eb54",
+    "url": "/Myweb/static/media/go-6.30bd42ea.svg"
+  },
+  {
+    "revision": "9d3ef70156bab3291227a1a290c425c2",
+    "url": "/Myweb/static/media/hal.9d3ef701.jpg"
+  },
+  {
+    "revision": "b38ff61aa327fbdc24f7b1dad7d04d1a",
+    "url": "/Myweb/static/media/heroku.b38ff61a.svg"
+  },
+  {
+    "revision": "18650de3ac75c6d12710985bc8db0eba",
+    "url": "/Myweb/static/media/hopscotch.18650de3.png"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/Myweb/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/Myweb/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "82291019f34ab54a677c456a84127cc3",
+    "url": "/Myweb/static/media/material-ui-1.82291019.svg"
+  },
+  {
+    "revision": "9e6424c4c8d51373630636a2718ea414",
+    "url": "/Myweb/static/media/misiki.9e6424c4.png"
+  },
+  {
+    "revision": "ddf5a022c3a29bcc8aa470ecedea8990",
+    "url": "/Myweb/static/media/misiki1.ddf5a022.png"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/Myweb/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "54d7bddcd3019ec3c3b23db00160ccef",
+    "url": "/Myweb/static/media/mssql.54d7bddc.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/Myweb/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "59dbf527a5eb57b8cc36cd25ee0f57d3",
+    "url": "/Myweb/static/media/orlogo.59dbf527.png"
+  },
+  {
+    "revision": "d7c985e7cca077dc2d77f89a66573e35",
+    "url": "/Myweb/static/media/postgresql.d7c985e7.svg"
+  },
+  {
+    "revision": "74c7da678ab10a20e91f4b5579a2e466",
+    "url": "/Myweb/static/media/profile.74c7da67.webp"
+  },
+  {
+    "revision": "146716bdf1a351db5705ec1e6a25ed08",
+    "url": "/Myweb/static/media/python.146716bd.svg"
+  },
+  {
+    "revision": "83fe61b05935f090b671b08aeb77b63d",
+    "url": "/Myweb/static/media/react-bootstrap.83fe61b0.svg"
+  },
+  {
+    "revision": "024196716802e272adb3cbefb81fd557",
+    "url": "/Myweb/static/media/react-router.02419671.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/Myweb/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/Myweb/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/Myweb/static/media/sass-1.b446ca5a.svg"
+  },
+  {
+    "revision": "41a5382aeb608a148ae89d694d34932b",
+    "url": "/Myweb/static/media/slide12.41a5382a.jpg"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "/Myweb/static/media/slide2.7256f309.webp"
+  },
+  {
+    "revision": "8e5b87b60151049a8df9ec0bd30978f0",
+    "url": "/Myweb/static/media/slide3.8e5b87b6.webp"
+  },
+  {
+    "revision": "6a4cf4853c511fba1f5b33b38e31f7cd",
+    "url": "/Myweb/static/media/slide4.6a4cf485.jpg"
+  },
+  {
+    "revision": "81c3311feff3f27fec087a944825b489",
+    "url": "/Myweb/static/media/slide5.81c3311f.jpg"
+  },
+  {
+    "revision": "e79a42b9281a3b30f1302d5452f9884f",
+    "url": "/Myweb/static/media/styled-components.e79a42b9.svg"
+  },
+  {
+    "revision": "dd86373ed9dec237abb4d9c34a2c6021",
+    "url": "/Myweb/static/media/tiktokclone1.dd86373e.png"
+  },
+  {
+    "revision": "3de182d2120823fe49c27eeb1765c53a",
+    "url": "/Myweb/static/media/typescript.3de182d2.svg"
+  },
+  {
+    "revision": "ed419638ef916666a989dfa868238b93",
+    "url": "/Myweb/static/media/vue.ed419638.png"
+  }
+]);
