09887c8fe8e32bf708210d0e617e069a5e7b3020
Pradeep Kumar Behera <43846840+cvrppradeep@users.noreply.github.com>
2026-04-09 16:16:00 +0530

    Update about.component.jsx
    change profile photo

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./about.styles.css";
-import Profile from "../../assets/img/profile/profile.webp";
+import Profile from "../../assets/img/profile/profile2.jpeg";
 import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
 
